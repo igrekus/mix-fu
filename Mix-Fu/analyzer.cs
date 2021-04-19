cb0c2c1bc4a3cddd3d013feae4b6c1a5717e4fc8
igrekus <upload234@mail.ru>
2021-04-19 18:39:16 +0300

    MISC: add todo

--- Mix-Fu/analyzer.cs
+++ Mix-Fu/analyzer.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Agilent.CommandExpert.ScpiNet.AgSCPI99_1_0;
 
 namespace Mixer
@@ -73,7 +73,7 @@ namespace Mixer
 
         public string SetPowerAttenuation(decimal att) => send(":POW:ATT " + att);
 
-        public string SetMeasCenterFreq(decimal freq) => send(":SENSe:FREQuency:CENTer " + freq);
+        public string SetMeasCenterFreq(decimal freq) => send(":SENSe:FREQuency:CENTer " + freq);   // TODO make work for both analyzers
 
         public string SetMarker1XCenter(decimal freq) => send(":CALCulate:MARKer1:X:CENTer " + freq);
 
